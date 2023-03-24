7da5fa389cbe931d44046854a1973e18a57f8467
Dmitry Mazin <dm@cyberdemon.org>
2023-03-24 15:35:39 +0000

    Updated version number to 0.2 in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chatgpt-commit-assistant",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     entry_points={"console_scripts": ["chatgpt-commit-assistant = chatgpt_commit_assistant.commit_assistant:main"]},
     install_requires=["openai", "GitPython"],
