cfb7953af733707fca450c4b3c1fe5b09fc63b05
Dmitry Mazin <dm@cyberdemon.org>
2023-03-27 21:36:38 +0100

    Updated package name and console script name in setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="chatgpt-commit-assistant",
+    name="git-lazy-commit",
     version="0.6",
     packages=find_packages(),
-    entry_points={"console_scripts": ["chatgpt-commit-assistant = assistant:main"]},
+    entry_points={"console_scripts": ["git-lazy-commit = assistant:main"]},
     install_requires=["openai", "GitPython"],
 )
