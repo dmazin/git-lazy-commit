a811b24d6614f9798ad47df2261589af4223b008
Dmitry Mazin <dm@archvile.net>
2023-03-28 10:20:58 +0100

    Update version number in setup.py from 0.8 to 0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="git-lazy-commit",
-    version="0.8",
+    version="0.9",
     packages=find_packages(),
     entry_points={"console_scripts": ["git-lazy-commit = assistant:main"]},
     install_requires=["openai", "GitPython"],
